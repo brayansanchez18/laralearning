10e877e620c44c2f4df842b214118f7a2708e169
Brayan Emanuel Sanchez Ramirez <brayan.sanchez.contacto@gmail.com>
2023-10-02 21:26:51 -0600

    instalacion de la plantilla admin lte para el administrador

---
+++ resources/views/admin/index.blade.php
@@ -0,0 +1,17 @@
+@extends('adminlte::page')
+
+@section('title', 'laralearning')
+
+@section('content_header')
+    <h1>Laralearning</h1>
+@stop
+
+@section('content')
+    <p>Welcome to this beautiful admin panel.</p>
+@stop
+
+@section('css')
+@stop
+
+@section('js')
+@stop
